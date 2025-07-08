c8c836fae10b369f8ed184607e1a3fded1a44666
hong.kim <hong.kim@bstarsolutions.com>
2025-07-08 10:40:27 +0700

    init

---
+++ src/test/java/com/xhk/grpc/GrpcClientCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhk.grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcClientCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
